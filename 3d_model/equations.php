1f3559f1e66ca07aac75cf91a855ec9ad72bc557
Neil Jackson <neiljackson1984@gmail.com>
2017-09-28 22:08:34 -0700

    checkpoint

--- 3d_model/equations.php
+++ 3d_model/equations.php
@@ -23,7 +23,21 @@ $c->lid->extent->z = 0.2 * $mm;
 
 $c->lidPocketOffset = 0.3 * $mm; //the gap between the walls of the lid pocket and the lid.
 
+$c->lidPocket->extent->x = $c->lid->extent->x + 2*$c->lidPocketOffset;
+$c->lidPocket->extent->y = $c->lid->extent->y + 2*$c->lidPocketOffset;
+$c->lidPocket->extent->z = $c->lid->extent->z;
+
 $c->wallThickness = 1.8*$mm;
+
+$c->extent->z = $c->lid->extent->z + $c->batteryCavity->extent->z + $c->wallThickness;
+$c->extent->x = max([
+	$c->batteryCavity->extent->x + 2*$c->wallThickness,
+	$c->lidPocket->extent->x + 2*$c->wallThickness
+]);
+$c->extent->y = max([
+	$c->batteryCavity->extent->y + 2*$c->wallThickness,
+	$c->lidPocket->extent->y + 2*$c->wallThickness
+]);
 
 unset($c);
 
